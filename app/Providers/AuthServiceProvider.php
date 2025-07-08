ae3ecc93674317cb2692c9f61b574e35a7eb132c
Piyawut <piyawut@mcrconsult.com>
2025-07-08 20:37:38 +0700

    [Requisition] fix error authService

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -10,22 +10,6 @@ class AuthServiceProvider extends ServiceProvider
     protected $policies = [
     ];
 
-    // public function boot(Gate $gate)
-    // {
-    //     $this->registerPolicies($gate);
-
-    //     // return if no permissions table -- for testing purpose
-    //     // if (! \Schema::connection('oracle_xxdev')->hasTable((new \App\Permission)->getTable())) {
-    //     //     return false;
-    //     // }
-
-    //     foreach ($this->getPermissions() as $permission) {
-    //         Gate::define($permission->name, function ($user) use ($permission) {
-    //             return $user->hasPermissionForCache($user, $permission->name);
-    //         });
-    //     }
-    // }
-
     public function boot(Gate $gate)
     {
         $this->registerPolicies($gate);
@@ -39,11 +23,11 @@ class AuthServiceProvider extends ServiceProvider
 
     private function getPermissions()
     {
-        $minutes = 3600; // 1 hr
-        $minutes = now()->addHours(24);
-        return \Cache::remember("permissions", $minutes, function () {
-            return \Packages\expense\app\Models\Permission::all();
-        });
+        // $minutes = 3600; // 1 hr
+        // $minutes = now()->addHours(24);
+        // return \Cache::remember("permissions", $minutes, function () {
+        //     return \Packages\expense\app\Models\Permission::all();
+        // });
         return \Packages\expense\app\Models\Permission::all();
     }
 }
