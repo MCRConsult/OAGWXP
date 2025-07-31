9b4fcfa35c1630350d0401d76d8716605b8eeef4
Piyawut <piyawut@mcrconsult.com>
2025-08-01 00:25:00 +0700

    [Report] fix bug

--- packages/expense/resources/views/report/requisition/pdf.blade.php
+++ packages/expense/resources/views/report/requisition/pdf.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    {{-- <link href="{{ public_path('css/pdf.css') }}" rel="stylesheet"> --}}
+     {{-- <link href="{{ public_path('css/pdf.css') }}" rel="stylesheet"> --}}
     <title> หลักฐานเอกสารส่งเบิก </title>
     <style type="text/css">
         @font-face {
