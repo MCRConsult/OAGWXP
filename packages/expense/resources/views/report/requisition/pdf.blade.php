c870653c054bf73ce3ecb05f52fd336e338068fe
Piyawut <piyawut@mcrconsult.com>
2025-08-01 00:54:31 +0700

    [Report] fix .

--- packages/expense/resources/views/report/requisition/pdf.blade.php
+++ packages/expense/resources/views/report/requisition/pdf.blade.php
@@ -4,6 +4,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     {{-- <link href="{{ asset('/public/css/pdf.css') }}" rel="stylesheet"> --}}
     <title> หลักฐานเอกสารส่งเบิก </title>
+    {{-- Style PDF --}}
     @include('expense::report.requisition._css')
 </head>
 <body>
