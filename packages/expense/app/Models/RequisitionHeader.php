0953cd77c640f24c0a6ac3b84004cd9549c786f3
Piyawut <piyawut@mcrconsult.com>
2025-08-04 13:08:09 +0700

    [Permission] fix condition for see data--req

--- packages/expense/app/Models/RequisitionHeader.php
+++ packages/expense/app/Models/RequisitionHeader.php
@@ -78,13 +78,13 @@ class RequisitionHeader extends Model
 
     public function scopeByRelatedUser($query)
     {
-        // $user = \Auth::user();
-        // $orgName = explode('_', $user->organizationV->name);
-
-        // return $query->where('document_category', 'like', '%'.$orgName[0].'%');
-
         $user = \Auth::user();
-        return $query->where('created_by', $user->id);
+        $orgName = explode('_', $user->organizationV->name);
+
+        return $query->where('document_category', 'like', '%'.$orgName[0].'%');
+
+        // $user = \Auth::user();
+        // return $query->where('created_by', $user->id);
     }
 
     public function getIsEnterAttribute($userId = null)
